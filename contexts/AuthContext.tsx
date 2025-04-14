c739dea523439dc624d71d35716da659088737ab
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2025-04-14 00:33:32 -0500

    fix: fixing errors in deps compatibility with expo react-native-async-storage
    fix other errors with importing types

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import type React from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Definir el tipo de usuario
